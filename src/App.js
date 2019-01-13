b20aee3a69e93a02fab32ffca257e67d9cdef84a
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2019-01-13 18:23:26 +0700

    Load font awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHeart as fasHeart,
+  faBookmark as fasBookmark
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faHeart as farHeart,
+  faComment as farComment,
+  faBookmark as farBookmark
+} from "@fortawesome/free-regular-svg-icons";
 
-const App = () => <div />;
+import Home from "./pages/Home";
+
+library.add([fasHeart, fasBookmark, farHeart, farComment, farBookmark]);
+
+const App = () => <Home />;
 
 export default App;
